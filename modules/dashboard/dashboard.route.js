f02fee22aa72955a76a5d25805dac0ef68ed2e47
Kavitha <kavitharanim@adityauniversity.in>
2026-05-11 11:18:10 +0530

    Changes in Feedback admin dashboard and sidebar

--- modules/dashboard/dashboard.route.js
+++ modules/dashboard/dashboard.route.js
@@ -4,5 +4,6 @@ const dashboardController = require('./dashboard.controller');
 const { protect, authorize } = require('../../middlewares/authMiddleware');
 
 router.get('/uniprime', protect, authorize('UNIPRIME'), dashboardController.getUniprimeDashboardData);
+router.get('/feedback', protect, authorize('FEEDBACK COORDINATOR', 'UNIPRIME', 'ADMIN'), dashboardController.getFeedbackDashboardData);
 
 module.exports = router;
