58837a03281680d98c2b08f6162f1f643fbdcce7
Kavitha <kavitharanim@adityauniversity.in>
2026-05-12 10:50:41 +0530

    Changes in Exam admin dashboard and student format

--- modules/dashboard/dashboard.route.js
+++ modules/dashboard/dashboard.route.js
@@ -5,5 +5,6 @@ const { protect, authorize } = require('../../middlewares/authMiddleware');
 
 router.get('/uniprime', protect, authorize('UNIPRIME'), dashboardController.getUniprimeDashboardData);
 router.get('/feedback', protect, authorize('FEEDBACK COORDINATOR', 'UNIPRIME', 'ADMIN'), dashboardController.getFeedbackDashboardData);
+router.get('/exam', protect, authorize('EXAMSECTION', 'UNIPRIME', 'ADMIN'), dashboardController.getExamDashboardData);
 
 module.exports = router;
