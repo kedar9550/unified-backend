670d289f292854af4e184a0830358d6af849658b
Kedarnath <kedarnadha@adityauniversity.in>
2026-05-14 05:45:07 +0530

    tb

--- modules/hodResearch/hodResearch.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, authorize } = require('../../middlewares/authMiddleware');
-const hodResearchController = require('./hodResearch.controller');
-
-router.get('/', protect, authorize('HOD', 'RESEARCH_DEAN', 'RESEARCH_COORDINATOR'), hodResearchController.getResearchRequests);
-
-module.exports = router;
